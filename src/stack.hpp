af08a0bd26296c8aee0f55381f32e5a0144c17a3
Milena Lazzaretti <mlazzare@student.s19.be>
2022-05-17 23:09:37 +0200

    stack

--- src/stack.hpp
+++ src/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: mlazzare <mlazzare@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 14:35:46 by mlazzare          #+#    #+#             */
-/*   Updated: 2022/05/17 20:43:02 by mlazzare         ###   ########.fr       */
+/*   Updated: 2022/05/17 23:09:05 by mlazzare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,33 +32,22 @@ namespace ft
 			bool				empty() const 				{	return c.empty();		};
 			size_type			size() const				{	return c.size(); 		};
 			value_type&			top() 						{	return c.back(); 		};
-			const value_type&	top() const 				{	return (c.back());		};
+			const value_type&	top() const 				{	return c.back();		};
 			void				push(const value_type& val) { 	c.push_back(val); 		};
 			void				pop() 						{	c.pop_back();			};
+			container_type		get_c()						{	return c;				};
 
-			friend bool			operator==(const stack<T,Container>& l, const stack<T,Container>& r)
-			{	return (l.c == r.c);	};
-
-			friend bool			operator<(const stack<T,Container>& l, const stack<T,Container>& r)
-			{	return (l.c < r.c);		};
-			
+			// [ RELATIONAL OPERATORS ]
+			friend bool operator == (stack const & l, stack const & r)				{		return l.c == r.c;			};
+			friend bool operator != (stack const & l, stack const & r)				{		return l.c != r.c;			};
+			friend bool operator < (stack const & l, stack const & r)				{		return l.c < r.c;			};
+			friend bool operator <= (stack const & l, stack const & r)				{		return l.c <= r.c;			};
+			friend bool operator > (stack const & l, stack const & r)				{		return l.c > r.c;			};
+			friend bool operator >= (stack const & l, stack const & r)				{		return l.c >= r.c;			};
 
 		protected:
 			container_type		c;
 	};
-
-	// [ NON MEMBER FUNCTIONS ]
-	template <class T, class Container>
-	bool						operator!=(const stack<T,Container>& l, const stack<T,Container>& r) { return (!(l == r)); }
-
-	template <class T, class Container>
-	bool						operator<=(const stack<T,Container>& l, const stack<T,Container>& r) { return (!(r < l)); }
-
-	template <class T, class Container>
-	bool						operator>(const stack<T,Container>& l, const stack<T,Container>& r) { return (r < l); }
-
-	template <class T, class Container>
-	bool						operator>=(const stack<T,Container>& l, const stack<T,Container>& r) { return (!(l < r)); }
-}
+};
 
 #endif
